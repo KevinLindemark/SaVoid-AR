230496894d79504125c2212ac426d091e7d56648
Kevin Lindemark <kevinlindemark@gmail.com>
2020-04-01 13:32:33 +0200

    first commit

---
+++ myPosition.js
@@ -0,0 +1,112 @@
+var options = {
+  enableHighAccuracy: true,
+  timeout: 27000,
+  maximumAge: 0
+};
+
+function success(pos) {
+// variable holds position coords
+  var crd = {lat: pos.coords.latitude, lon: pos.coords.longitude};
+  console.log("crd: ", crd);
+  sendMessage(crd);
+  receiveMessage(crd);
+   
+/*
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+  */
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+/************** LOCATION END ***** MQTT START ********/
+// trying to see if I can send and recive position coords via MQTT
+// Setup
+const broker = "influx.itu.dk";
+const port = 9002;
+const secured = true;
+const topic = "ituF2020/EXPD/DI-SaVoid";
+let clientId;
+
+/*
+if(!document.cookie){
+    clientId = "clientID_" + parseInt(Math.random() * 1000);
+    document.cookie = "username=",clientId;    
+}
+*/
+clientId = "clientID_" + parseInt(Math.random() * 1000);
+//console.log("cookieAsId ");
+ // console.log(document.cookie);
+ // let cookieAsId = document.cookie; 
+
+
+
+// Create a client instance
+let client = new Paho.MQTT.Client(broker, Number(port), clientId);
+console.log("let client: ");
+console.log(client);
+
+// connect the client
+client.connect({
+    onSuccess: onConnect,
+    useSSL: secured,
+});
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = receiveMessage;
+
+// called when the client connects
+function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    client.subscribe(topic);
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost: " + responseObject.errorMessage);
+    }
+}
+
+// call to send a message
+function sendMessage(msg) {
+   // document.cookie = "username=" + clientIdReceived;    
+    console.log("sendMessage: " )
+    console.log(msg);
+    let mObj = { deviceId: clientId, content: msg };
+    let mSend = new Paho.MQTT.Message(JSON.stringify(mObj));
+    mSend.destinationName = topic;
+    console.log("mSend: ");
+    console.log(mSend);
+    client.send(mSend);
+}
+
+// called when a message arrives
+function receiveMessage(msg) {
+    console.log("msg :");
+    console.log(msg);
+    let mUnpack = JSON.parse(msg.payloadString);
+  console.log("mUnpack: ");
+  console.log(mUnpack);
+    let receivedMessage = mUnpack.content;
+    let clientIdReceived = mUnpack.deviceId;
+   console.log("clientRecieved: ");
+  console.log(clientIdReceived);
+   
+    //let userCoords = [lat, lon];
+    console.log("recievedMessage: ")
+    console.log(receivedMessage);
+    
+  //console.log("cookieAsId ");
+  //console.log(document.cookie);
+  
+
+}
---
+++ places.js
@@ -0,0 +1,108 @@
+
+// getting places from APIs
+/*
+async function loadPlaces(position) {
+    console.log("position loadPlaces: ")
+    console.log(position)
+    const params = {
+        radius: 300,    // search places not farther than this value (in meters)
+        clientId: 'YXHLAEFCNT0YYPIWLOK51A52ADHWADMPFYY4XCKBAALJCRRK',
+        clientSecret: '3ZEWUUZMNOLZJ0YIWGI3P1JW3WL1N3XUIODI2W5Z12WAPD0A',
+        version: '20300101',    // foursquare versioning, required but unuseful for this demo
+    };
+
+    // CORS Proxy to avoid CORS problems
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+    // Foursquare API (limit param: number of maximum places to fetch)
+    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+            &ll=${position.latitude},${position.longitude}
+            &radius=${params.radius}
+            &client_id=${params.clientId}
+            &client_secret=${params.clientSecret}
+            &limit=30 
+            &v=${params.version}`;
+    try {
+        const res = await fetch(endpoint);
+        const resp = await res.json();
+        return resp.response.venues;
+    }
+    catch (err) {
+        console.error('Error with places API', err);
+    }
+};
+*/
+function loadPlaces(position) {
+    // console.log("position loadPlaces: ")
+    //console.log(position)
+    receiveMessage(position);
+    position = position.longitude, position.latitude;
+    console.log("position");
+    console.log(position);
+};
+
+
+window.onload = () => {
+    const scene = document.querySelector('a-scene');
+        
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
+
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position)
+            .then((places) => {
+                places.forEach((place) => {
+                    
+                    // standard latlong
+                    //const latitude = place.location.lat;
+                    //const longitude = place.location.lng;
+                    // MQTT
+                    const latitude = place.crd.lat;
+                    const longitude = place.crd.lon;
+                    console.log("Places: latitude: ")
+                    console.log(latitude);
+
+
+                    const entity = document.createElement('a-entity');
+                    entity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    entity.setAttribute('look-at', '[gps-camera]');
+                    entity.setAttribute('scale', '20 20 20');
+
+                    const markerEl = document.createElement('a-image');
+                    
+                    markerEl.setAttribute('src', '../hello.png');
+
+                    entity.appendChild(markerEl);
+                    
+                   //const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+                    //console.log(distanceMsg);   // "890 meters"
+                    const distanceMsg = "73";
+
+                    // add text for distance in meters
+                    const textEl = document.createElement('a-entity');
+                   
+                    textEl.setAttribute('text', {
+                        color: 'pink',
+                        align: 'center',
+                        width: 4,
+                        value: `${place.name} m. ${distanceMsg}`,
+                    });
+
+                    textEl.setAttribute('position', '0 -0.75 0');
+                    
+                    entity.appendChild(textEl);
+                    scene.appendChild(entity);
+
+                });
+
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
+};
+
